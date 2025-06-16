2fa6a9c1a3b179953a04cdbce14a8e20b0023c28
kekneus373 <klu4k0@protonmail.com>
2025-06-16 18:10:58 +0300

    Fixed a file overwriting bug
    add "-trimmed" at the end of the filename

--- ffmpeg-trim.sh
+++ ffmpeg-trim.sh
@@ -8,6 +8,6 @@ read -p "Enter the start point (e.g. 00:00:10): " ss
 read -p "Enter the end point (e.g. 00:00:30): " t
 
 # Run ffmpeg command with user input
-ffmpeg -i "$input_file" -ss "$ss" -t "$t" -c:v libx264 -c:a aac "${input_file%.*}.mp4"
+ffmpeg -i "$input_file" -ss "$ss" -t "$t" -c:v libx264 -preset faster -c:a aac "${input_file%.*}-trimmed.mp4"
 
 echo "Processing complete."
