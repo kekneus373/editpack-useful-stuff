9e42255443185ddd2281f0316f71173dbda606d2
kekneus373 <klu4k0@protonmail.com>
2026-01-09 15:13:29 +0200

    fix issue #3
    https://github.com/kekneus373/editpack-useful-stuff/issues/3

---
+++ aexciter-video-folder.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Enter working directory
+echo "Full path of working directory: "; read dir
+cd "$dir" || { echo "Directory not found!"; exit 1; }
+
+# Prompt the user for input
+read -p "Source files extension (e.g. mkv): " extin
+
+# Output directory
+mkdir -p distorted
+
+# Ask about which codec to use
+echo "Choose audio codec:"
+select profile in "AAC - for MP4 videos" "MP3 - very flexible" "WAV - clean sound" "OGG - open source"; do
+    case $REPLY in
+        1) c=aac; break ;;
+        2) c=mp3; break ;;
+        3) c=pcm_s16le; break ;;
+        4) c=libvorbis; break ;;
+        *) echo "Invalid option." ;;
+    esac
+done
+
+read -p "Set the audio bitrate (e.g. 128k): " ba
+read -p "Set the audio sample rate (e.g. 44100): " ar
+
+# Run ffmpeg command to take the action for every file there
+for i in *."$extin"; do ffmpeg -i "$i" -af "aexciter=level_in=14.464:level_out=1.984:amount=1:drive=8.5:blend=0:freq=7500:ceil=9999" -c:v copy -c:a "$c" -b:a "$ba" -ar "$ar" distorted/"$i"; done
+
+echo "================ Processing complete ================"
